8decfb0188bd8fc6770e2101895fc40c31b5f47a
ATS88GR <turok.artyom@gmail.com>
2023-03-18 11:47:06 +0300

    delete TempEmployeeList, Rebuild LoadFile and SaveFile

--- LoadFile.java
+++ LoadFile.java
@@ -2,13 +2,13 @@ package ExamTaskV2;
 
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.util.List;
 
 public class LoadFile {
 
     public static void load (String path, EmployeeList loadEmpList) {
         try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path))) {
-            TempEmployeeList tel = (TempEmployeeList) ois.readObject();
-            loadEmpList.setBaseList(tel.getTempBaseList()); // read List
+            loadEmpList.setBaseList(((EmployeeList) ois.readObject()).getBaseList()); // read List
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
